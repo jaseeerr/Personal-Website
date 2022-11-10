1b3b8554fd8278942e5fc15334d22cb619cbcd2d
jaseeerr <90355055+jaseeerr@users.noreply.github.com>
2022-11-10 18:40:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,3 +250,122 @@
   new PureCounter();
 
 })()
+
+
+
+
+function namecheck()
+    {
+        let name=document.getElementById("name").value
+        let nm=null
+         nm = name.trim()
+      
+       var spc=/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+[A-Za-z]*$/;
+        document.getElementById("name").value = nm;
+        if(nm==""||name==null)
+        {
+            document.getElementById("nmlabel").style.display="block"
+            return false
+        }
+        else if(!isNaN(nm[0]))
+        {
+            document.getElementById("nmlabel").style.display="none"
+            document.getElementById("nmlabel-01").style.display="block"
+            return false
+        }
+        else if(nm.match(spc))
+        {
+            document.getElementById("nmlabel").style.display="none" 
+            document.getElementById("nmlabel-01").style.display="none"  
+            document.getElementById("nmlabel-02").style.display="block"
+            return false
+        }
+        else{
+            document.getElementById("nmlabel").style.display="none" 
+            document.getElementById("nmlabel-01").style.display="none" 
+            document.getElementById("nmlabel-02").style.display="none"
+            return true
+    
+        }
+    }
+    
+    
+    function emailcheck()
+    {
+        var val=document.getElementById("email").value
+        var v=null
+         v = val.trim()
+        document.getElementById("email").value = v;
+        if(v==""||val==null)
+        {
+            document.getElementById("el").style.display="block"
+            return false
+        }
+        else if(!isNaN(v[0]))
+        {
+            document.getElementById("el-01").style.display="block"
+            return false
+        }
+    
+    
+        else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v))
+      {
+        document.getElementById("el-02").style.display="block"
+        return false
+      }
+        // alert("You have entered an invalid email address!")
+        // return (false)
+    
+    
+    
+    
+    
+        else{
+            document.getElementById("el").style.display="none" 
+            document.getElementById("el-01").style.display="none"
+            document.getElementById("el-02").style.display="none" 
+            return true
+    
+        }
+    }
+    
+    function msgcheck()
+    {
+    
+        var val=document.getElementById("msg").value
+        var v=null
+         v = val.trim()
+        
+        if(v==""||val==null)
+        {
+            document.getElementById("ml").style.display="block"
+            return false
+        }
+       
+        else{
+            document.getElementById("ml").style.display="none" 
+            document.getElementById("ml-01").style.display="none"   
+            return true
+    
+        }
+    }
+
+    function checksubject()
+    {
+      var subject = document.getElementById("subject").value
+      var filter = null
+      filter = subject.trim()
+
+      if(subject==""||filter==null)
+      {
+          document.getElementById("es").style.display="block"
+          return false
+      }
+     
+      else{
+          document.getElementById("ml").style.display="none" 
+          document.getElementById("ml-01").style.display="none"   
+          return true
+  
+      }
+    }
